9b2da9f9ebd68c312cdb9d946925ae7745c3462b
lee-serz <serzh.li.04.06@yandex.ru>
2025-02-07 19:16:50 +0300

    add: user model

---
+++ src/user/user.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Param,
+  Patch,
+  Put,
+  Get,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { CurrentUser } from 'src/auth/decorators/user.decorator';
+import { UserDto } from './dto/user.dto';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('profile')
+  @Auth()
+  async getProfile(@CurrentUser('id') id: number) {
+    return this.userService.byId(id);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Auth()
+  @Put('profile')
+  async login(@CurrentUser('id') id: number, @Body() dto: UserDto) {
+    return this.userService.updateProfile(id, dto);
+  }
+
+  @HttpCode(200)
+  @Auth()
+  @Patch('profile/favorites/:productId')
+  async toggleFavorite(
+    @Param('productId') productId: string,
+    @CurrentUser('id') id: number,
+  ) {
+    return this.userService.toggleFavorite(id, +productId);
+  }
+  // get profile
+  // toggleFavorites
+  // updateProfile
+}
